66f6d49f9c9cd50fa7782fdac966e4f46cc1a9c0
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-06-13 23:45:53 +0300

    add geocoding api request to get latitude and longitude

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,8 +2,20 @@ import request from 'request'
 
 const url = 'https://api.weatherstack.com/current?access_key=17a911fce50765313671cede79dba549&query=Kyiv'
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true}, (error, response) => {
+    const currentWeather = response.body.current;
+    const temperature = currentWeather.temperature;
+    const feelsLike = currentWeather.feelslike;
+    const weatherDescription = currentWeather.weather_descriptions[0]
+    console.log(`It is ${weatherDescription}, it is currently ${temperature} degrees now, it feels like ${feelsLike} degrees`);
 })
 
+const urlGeocoding = 'https://api.api-ninjas.com/v1/geocoding?city=Kyiv'
+request({url: urlGeocoding, headers: {'X-Api-Key': 'BYlSI+tEKe67k/9pKkM9UQ==5ZjYrhvUW5dwfuG8'},
+ json: true}, (error, response) => {
+    const data = response.body;
+    const city = data[0];
+    const latitude = city.latitude;
+    const longitude = city.longitude;
+    console.log(latitude, longitude)
+ })
