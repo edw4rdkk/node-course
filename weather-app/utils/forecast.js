b291194173b495074b3fa222d83e5a53fa98fd12
Eduard Kyrychenko <eduardkyrychenko923@gmail.com>
2025-06-17 21:08:34 +0300

    add reusable forecast function

---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,21 @@
+import request from "request";
+
+export const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.weatherstack.com/current?access_key=17a911fce50765313671cede79dba549&query= ${latitude},${longitude}`;
+
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('[forecast] unable to connect to weather service', undefined)
+        } else if (response.body.error) {
+            callback('[forecast] unable to find location', undefined)
+        } else {
+            const currentWeather = response.body.current;
+            const temperature = currentWeather.temperature;
+            const feelsLike = currentWeather.feelslike;
+            const weatherDescription = currentWeather.weather_descriptions[0]
+            const message = `[forecast] It is ${weatherDescription}, it is currently ${temperature} degrees, it feels like ${feelsLike} degrees`
+            callback(undefined, message)
+        }
+    })
+
+}
