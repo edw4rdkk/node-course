a9a2748b986e08a01da175a7e4353e332a2209f3
Eduard Kyrychenko <eduardkyrychenko923@gmail.com>
2025-06-17 15:42:42 +0300

    import geocode function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,36 +1,36 @@
 import request from 'request'
-import { geocode } from './utils/geocode'
+import { geocode } from './utils/geocode.js'
 
-const url = 'https://api.weatherstack.com/current?access_key=17a911fce50765313671cede79dba549&query=Kyiv'
+// const url = 'https://api.weatherstack.com/current?access_key=17a911fce50765313671cede79dba549&query=Kyiv'
 
-request({ url: url, json: true}, (error, response) => {
-    if (error) {
-        console.log('unable to connect to weather service')
-    } else if (response.body.error) {
-        console.log('unable to find location')
-    } else {
-        const currentWeather = response.body.current;
-        const temperature = currentWeather.temperature;
-        const feelsLike = currentWeather.feelslike;
-        const weatherDescription = currentWeather.weather_descriptions[0]
-        console.log(`It is ${weatherDescription}, it is currently ${temperature} degrees, it feels like ${feelsLike} degrees`);
-    }
-})
+// request({ url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('unable to connect to weather service')
+//     } else if (response.body.error) {
+//         console.log('unable to find location')
+//     } else {
+//         const currentWeather = response.body.current;
+//         const temperature = currentWeather.temperature;
+//         const feelsLike = currentWeather.feelslike;
+//         const weatherDescription = currentWeather.weather_descriptions[0]
+//         console.log(`It is ${weatherDescription}, it is currently ${temperature} degrees, it feels like ${feelsLike} degrees`);
+//     }
+// })
 
-const urlGeocoding = 'https://api.api-ninjas.com/v1/geocoding?city=Kyiv'
-request({url: urlGeocoding, headers: {'X-Api-Key': 'BYlSI+tEKe67k/9pKkM9UQ==5ZjYrhvUW5dwfuG8'},
- json: true}, (error, response) => {
-    if (error) {
-        console.log('unable to connect to location service')
-    } else if (response.body.error) {
-        console.log('no matching results')
-    } else {
-    const data = response.body;
-    const city = data[0];
-    const latitude = city.latitude;
-    const longitude = city.longitude;
-    console.log(latitude, longitude)
- }})
+// const urlGeocoding = 'https://api.api-ninjas.com/v1/geocoding?city=Kyiv'
+// request({url: urlGeocoding, headers: {'X-Api-Key': 'BYlSI+tEKe67k/9pKkM9UQ==5ZjYrhvUW5dwfuG8'},
+//  json: true}, (error, response) => {
+//     if (error) {
+//         console.log('unable to connect to location service')
+//     } else if (response.body.error) {
+//         console.log('no matching results')
+//     } else {
+//     const data = response.body;
+//     const city = data[0];
+//     const latitude = city.latitude;
+//     const longitude = city.longitude;
+//     console.log(latitude, longitude)
+//  }})
 
 geocode('Kyiv', (error, data) => {
     console.log('Error', error)
