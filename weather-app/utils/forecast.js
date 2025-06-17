32a2bc8b454c0aecf87557048af2d6b786287a36
Eduard Kyrychenko <eduardkyrychenko923@gmail.com>
2025-06-18 00:57:33 +0300

    refactoring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ import request from "request";
 export const forecast = (latitude, longitude, callback) => {
     const url = `https://api.weatherstack.com/current?access_key=17a911fce50765313671cede79dba549&query= ${latitude},${longitude}`;
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body} = {}) => {
         if (error) {
             callback('[forecast] unable to connect to weather service', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('[forecast] unable to find location', undefined)
         } else {
-            const currentWeather = response.body.current;
+            const currentWeather = body.current;
             const temperature = currentWeather.temperature;
             const feelsLike = currentWeather.feelslike;
             const weatherDescription = currentWeather.weather_descriptions[0]
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,15 +3,17 @@ import request from "request"
 export const geocode = (address, callback) => {
     const url = `https://api.api-ninjas.com/v1/geocoding?city=${address}`
 
-    request({ url: url, headers: {'X-Api-Key': 'BYlSI+tEKe67k/9pKkM9UQ==5ZjYrhvUW5dwfuG8'}, json: true}, (error, response) => {
+    request({ url, headers: {'X-Api-Key': 'BYlSI+tEKe67k/9pKkM9UQ==5ZjYrhvUW5dwfuG8'}, json: true}, (error, {body} = {}) => {
+        console.log(body)
         if(error) {
-            callback('Unable to connect to loction services', undefined)
-        } else if (response.body.error) {
-            callback('unable to find location. try another search', undefined)
+            callback('[geocode] Unable to connect to loction services', undefined)
+        } else if (body.error) {
+            callback('[geocode] unable to find location. try another search', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body[0].latitude,
-                longitude: response.body[0].longitude
+                latitude: body[0].latitude,
+                longitude: body[0].longitude,
+                location: body[0].name
             })
         }
     })
