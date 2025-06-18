517c4dca3a623fe577be5c0973bc4ef8c39ad10f
Eduard Kyrychenko <eduardkyrychenko923@gmail.com>
2025-06-19 01:42:01 +0300

    add static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,20 @@
 import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('Hello express')
-})
-
-app.get('/help', (req, res) => {
-    res.send('help page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('about the website')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
-    res.send('showing weather')
+    res.send({
+        forecast: 'cold',
+        location: 'Kyiv'
+    })
 })
 
 app.listen(3000, () => {
